98ac467065b648564fe9c4c4e6c4268bbf3e0f06
(cdep)illabout <cdep.illabout@gmail.com>
2019-03-03 21:49:40 +0900

    Initial commit.

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for type-level-stuff
+
+## Unreleased changes
---
+++ test/Doctest.hs
@@ -0,0 +1,13 @@
+module Main
+  ( main
+  )
+where
+
+import Test.DocTest
+
+main :: IO ()
+main = doctest $ ["src"] <> ghcArgs
+
+ghcArgs :: [String]
+ghcArgs = []
+
